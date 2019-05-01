f9dafea1da839e6d333bca44a48489d4c80f8f4b
algebraic-animation-3644 <nathanchan1007@gmail.com>
2019-05-01 07:38:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ function nowServing(katzDeliLine) {
     if (katzDeliLine.length < 1) {
         return "There is nobody waiting to be served!"
     } else {
-        return "Currently serving" + " " + katzDeliLine[0] + ".";
+        katzDeliLine.shift();
+        return "Currently serving" + " " + katzDeliLine[0] + ".", console.log(katzDeliLine);
 
     }
 }
