d0fc12f264edd60eb7f8cc9aea76ee190bdd4847
algebraic-animation-3644 <nathanchan1007@gmail.com>
2019-05-01 07:33:51 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,27 @@
+function takeANumber(line,name) {
+    line.push(name);
+    return "Welcome," + " " + name + ". " + "You are number " + line.length + " in line."
+}
+function nowServing(katzDeliLine) {
+    if (katzDeliLine.length < 1) {
+        return "There is nobody waiting to be served!"
+    } else {
+        return "Currently serving" + " " + katzDeliLine[0] + ".", katzDeliLine.slice(1,length);
+
+    }
+}
+function currentLine(katzDeliLine) {
+    var i = 0;
+    var j = 1;
+    var string = "";
+    if (katzDeliLine.length < 1) {
+        return "The line is currently empty."
+    } else {
+        while (i < katzDeliLine.length-1) {
+            string = string + j + ". " + katzDeliLine[i] + ", ";
+            i++;
+            j++;
+        }
+    }
+    return "The line is currently: " + string + j + ". " + katzDeliLine[i];
+}
